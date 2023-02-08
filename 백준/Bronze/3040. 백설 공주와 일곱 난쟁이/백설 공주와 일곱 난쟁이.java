e97683fbb32989e9ed8adadc134214a19534e1b4
wjdgusaho <56811267+wjdgusaho@users.noreply.github.com>
2023-02-08 15:25:05 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 	
 	static int[] numbers, inputs;
 	static int T , P;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		T = 9;
 		P = 7;
@@ -14,7 +17,7 @@ public class Main {
 		inputs = new int[T];
 		
 		for(int t = 0; t < T; t++) {
-			inputs[t] = sc.nextInt();
+			inputs[t] = Integer.parseInt(br.readLine());
 		}
 		
 		combi(0, 0);
