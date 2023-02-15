2852fb93e2e2b9c2ab21100403f88e97a06c8098
wjdgusaho <56811267+wjdgusaho@users.noreply.github.com>
2023-02-15 14:15:36 +0900

    [Silver I] Title: 절댓값 힙, Time: 348 ms, Memory: 26528 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+			if (Math.abs(o1) == Math.abs(o2))
+				return o1 - o2;
+			return Math.abs(o1) - Math.abs(o2);
+		});
+
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			if (num == 0) {
+				if (!pq.isEmpty())
+					bw.write(pq.poll() + "\n");
+				else
+					bw.write(0 + "\n");
+			} else
+				pq.add(num);
+		}
+		bw.close();
+	}
+}
