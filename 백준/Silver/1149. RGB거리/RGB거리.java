7d4c15901807d29467c85baebc67656a694ab212
wjdgusaho <56811267+wjdgusaho@users.noreply.github.com>
2023-03-29 03:05:11 +0900

    [Silver I] Title: RGB거리, Time: 156 ms, Memory: 15712 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,49 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-import javax.swing.InputMap;
-
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] memori = new int[n][3]; // 각 집을 칠할 때 최소 비용을 저장하는 배열
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+        // 첫번째 집을 칠할 때 비용 입력
+        int[] min = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        memori[0][0] = min[0];
+        memori[0][1] = min[1];
+        memori[0][2] = min[2];
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i < n; i++) {
+            // 현재 집을 빨간색으로 칠했을 때의 최소 비용
+            min = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            memori[i][0] = Math.min(memori[i-1][1], memori[i-1][2]) + min[0];
+            // 현재 집을 초록색으로 칠했을 때의 최소 비용
+            memori[i][1] = Math.min(memori[i-1][0], memori[i-1][2]) + min[1];
+            // 현재 집을 파란색으로 칠했을 때의 최소 비용
+            memori[i][2] = Math.min(memori[i-1][0], memori[i-1][1]) + min[2];
+        }
 
-		int[] memori = new int[N];
-
-		for (int n = 0; n < N; n++) {
-			int[] min = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			int[] idx = new int[3];
-			if (n == 0) {
-				for (int i = 0; i < 3; i++) {
-					memori[i] = min[i];
-					idx[i] = i;
-				}
-				continue;
-			} else {
-				int[] tmp = new int[3];
-				for (int i = 0; i < 3; i++) {
-					tmp[i] = Integer.MAX_VALUE;
-				}
-
-				for (int i = 0; i < 3; i++) {
-					for (int j = 0; j < 3; j++) {
-						if (i == j)
-							continue;
-						tmp[j] = Math.min(tmp[j], memori[i] + min[j]);
-					}
-				}
-				memori = tmp;
-			}
-		}
-//		System.out.println(Arrays.toString(memori));
-
-		int anw = Integer.MAX_VALUE;
-		for (int i = 0; i < 3; i++) {
-			anw = Math.min(anw, memori[i]);
-		}
-		System.out.println(anw);
-	}
-
+        // 마지막 집을 각각 빨간색, 초록색, 파란색으로 칠했을 때의 비용 중 최소값을 출력
+        int anw = Math.min(memori[n-1][0], Math.min(memori[n-1][1], memori[n-1][2]));
+        System.out.println(anw);
+    }
 }
