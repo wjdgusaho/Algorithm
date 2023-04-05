0c66da1c8573dba38d39e650e4b5d77a4feda46c
wjdgusaho <56811267+wjdgusaho@users.noreply.github.com>
2023-04-05 12:05:14 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 248 ms, Memory: 27,684 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int N, B, map[], numbers[], ans, min;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		for(int tc = 1; tc <= TC; tc++) {
+			
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			ans = 0;
+			min = Integer.MAX_VALUE;
+			//배열 정렬 
+			Arrays.sort(map);
+			
+			for(int i = 1; i <= N; i++) {
+				numbers = new int[i];
+				comb(0, 0, i);
+			}
+			
+			ans = min;
+			System.out.println("#"+tc+" "+ans);
+		}
+		
+	}
+	
+	static void comb(int start, int cnt, int f){
+		
+		if(cnt == f) {
+			//System.out.println(Arrays.toString(numbers));
+			int sum = 0;
+			for(int i = 0; i < f; i++) {
+				sum += numbers[i];
+			}
+			if(sum >= B) {
+				sum = sum - B;
+				min = Math.min(sum, min);
+				//System.out.println(Arrays.toString(numbers));
+			}
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			numbers[cnt] = map[i];
+			comb(i+1, cnt+1, f);
+		}
+	}
+	
+}
