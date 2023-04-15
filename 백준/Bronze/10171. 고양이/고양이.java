6c3ccb818196078c185beb003cce8e4502e5e7c6
wjdgusaho <56811267+wjdgusaho@users.noreply.github.com>
2023-04-15 23:31:11 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|");
+		
+		System.out.println(sb.toString());
+		
+	}
+}
