a7a538f50bea3bc050bb4e0ac62e29c05b320b31
정현모 <wjdgusaho@naver.com>
2023-12-11 21:03:14 +0900

    [Silver III] Title: N과 M (1), Time: 2208 ms, Memory: 58364 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,62 +1,42 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
+    static int N, M;
+    static int[] inputs, numbers;
+    static boolean[] visited;
+    public static void main(String[] aregs){
 
-	static int N;
-	static int M;
-	static boolean[] visited;
-	static int[] numbers, inputs;
-	
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		//숫자 갯수
-		N = in.nextInt();
-		//선택하는 갯수
-		M = in.nextInt();
-		
-		//숫자를 사용하고있는가?
-		visited = new boolean[N];
-		
-		//선택한 값이 들어갈 배열
-		numbers = new int[M];
-		
-		//입력한 값의 배열
-		inputs = new int[N];
-		
-		//숫자 추가
-		for(int i = 0; i < N; i++) {
-			inputs[i] = i+1;
-		}
-		perm(0);
-	}
-    
-	private static void perm(int cnt) {
-		
-		if(cnt >= M) {
-			for(int i = 0; i < M; i++) {
-				System.out.print(numbers[i]+" ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i = 0; i < N; i++) {
-			
-			//값을 사용중인지 체크 하고 사용중이면 continue
-			if(visited[i]) continue;
-			
-			//값을 사용중이지 않았으니 사용중으로 변경
-			visited[i] = true;
-			
-			//numbers에 visited값 
-			numbers[cnt] = inputs[i];
-			
-			perm(cnt + 1);
-			
-			//peam 끝나고 돌아왔으니 사용하지 않음으로 변경
-			visited[i] = false;
-				
-		}	
-	}
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        inputs = new int[N];
+        numbers = new int[M];
+        visited = new boolean[N];
+
+        for(int i = 1; i <= N; i++){
+            inputs[i-1] = i;
+        }
+
+        perm(0);
+
+    }
+
+    public static void perm(int cnt){
+
+        if(cnt == M){
+            for(int i = 0; i < M; i++){
+                 System.out.print(numbers[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            numbers[cnt] = inputs[i];
+            perm(cnt + 1);
+            visited[i] = false;
+        }
+    }
 }
