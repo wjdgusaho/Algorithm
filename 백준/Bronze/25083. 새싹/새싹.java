d5ed6002313c268ed927ca68240cdd8ef03c4f5e
정현모 <wjdgusaho@naver.com>
2024-01-10 23:19:01 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |");
+		
+		System.out.println(sb);
+	};
+}
