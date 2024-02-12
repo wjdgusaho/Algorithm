822b44e4f7371102a478483762507eedd4f010f9
정현모 <wjdgusaho@naver.com>
2024-02-12 22:11:12 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("|\\_/|\n");
+		sb.append("|q p|   /}\n");
+		sb.append("( 0 )\"\"\"\\\n");
+		sb.append("|\"^\"`    |\n");
+		sb.append("||_/=\\\\__|\n");
+		System.out.println(sb);
+	}
+}
