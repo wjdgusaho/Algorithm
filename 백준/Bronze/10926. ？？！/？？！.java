e86b11b61cf83c89d53c207f275916949d1d03ca
정현모 <wjdgusaho@naver.com>
2024-02-13 23:24:44 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String tmp = sc.nextLine();
+		System.out.println(tmp+"??!");
+	}
+
+}
