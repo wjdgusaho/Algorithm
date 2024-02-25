ef03e66c846dd7743b5955b425073470cdbcd70c
정현모 <wjdgusaho@naver.com>
2024-02-25 23:34:58 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 200 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < N; i++) {
+			for(int j = N; j > i; j--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
