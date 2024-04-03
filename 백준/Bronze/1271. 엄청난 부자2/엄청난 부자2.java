fdcc6fd48876a37388e4d9ee9e7c95bd7a14247e
정현모 <wjdgusaho@naver.com>
2024-04-03 15:30:54 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 244 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = sc.nextBigInteger();
+		BigInteger B = sc.nextBigInteger();
+		
+		System.out.println(A.divide(B) +"\n" + A.remainder(B));
+	}
+}
